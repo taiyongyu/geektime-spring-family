c42cdb83d2be0d400cee9c07a870cb16facdfb2b
taiyongyu <372858845@qq.com>
2019-04-24 15:14:08 +0800

    redis

--- Chapter 4/redis-demo/src/main/java/geektime/spring/springbucks/SpringBucksApplication.java
+++ Chapter 4/redis-demo/src/main/java/geektime/spring/springbucks/SpringBucksApplication.java
@@ -31,6 +31,10 @@ public class SpringBucksApplication implements ApplicationRunner {
 		SpringApplication.run(SpringBucksApplication.class, args);
 	}
 
+	/**
+	 * spring 中的redisTemplate默认是redisTemplate<Object>的，不符合实际需求
+	 * 因此，要手动声明一个符合实际类型的RedisTemplate<String, Coffee>
+	 */
 	@Bean
 	public RedisTemplate<String, Coffee> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
 		RedisTemplate<String, Coffee> template = new RedisTemplate<>();
@@ -38,20 +42,24 @@ public class SpringBucksApplication implements ApplicationRunner {
 		return template;
 	}
 
+	/**
+	 * 利用回调配置类，配置从主节点读取数据。(此配置针对redis集群，本例中只是单点模式，这里只是做一个演示了解即可)
+	 */
 	@Bean
 	public LettuceClientConfigurationBuilderCustomizer customizer() {
+
 		return builder -> builder.readFrom(ReadFrom.MASTER_PREFERRED);
 	}
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
+		// 第一次是从数据库中查询的，然后放入redis中
 		Optional<Coffee> c = coffeeService.findOneCoffee("mocha");
 		log.info("Coffee {}", c);
-
+        // 直接从redis中获取数据
 		for (int i = 0; i < 5; i++) {
 			c = coffeeService.findOneCoffee("mocha");
 		}
-
 		log.info("Value from Redis: {}", c);
 	}
 }
--- Chapter 4/redis-demo/src/main/java/geektime/spring/springbucks/service/CoffeeService.java
+++ Chapter 4/redis-demo/src/main/java/geektime/spring/springbucks/service/CoffeeService.java
@@ -22,6 +22,10 @@ public class CoffeeService {
     private static final String CACHE = "springbucks-coffee";
     @Autowired
     private CoffeeRepository coffeeRepository;
+    /**
+     * coffee的名字作为key，Coffee作为value 放入缓存中
+     * 需要为redisTemplate指明key和value的类型
+     */
     @Autowired
     private RedisTemplate<String, Coffee> redisTemplate;
 
@@ -30,11 +34,16 @@ public class CoffeeService {
     }
 
     public Optional<Coffee> findOneCoffee(String name) {
+        // 第一个参数对应redis中的key，它的value是一个map。第二个参数是map中的key，第三个参数是map中的value
+        // 取出redis中的所有hashmap对象
         HashOperations<String, String, Coffee> hashOperations = redisTemplate.opsForHash();
+        // 判断redis和hashmap中是否存在相应的key
+        // 如果存在，就直接返回
         if (redisTemplate.hasKey(CACHE) && hashOperations.hasKey(CACHE, name)) {
             log.info("Get coffee {} from Redis.", name);
             return Optional.of(hashOperations.get(CACHE, name));
         }
+        // 如果不存在，到数据库中去查找再放入redis里面。
         ExampleMatcher matcher = ExampleMatcher.matching()
                 .withMatcher("name", exact().ignoreCase());
         Optional<Coffee> coffee = coffeeRepository.findOne(
@@ -42,7 +51,9 @@ public class CoffeeService {
         log.info("Coffee Found: {}", coffee);
         if (coffee.isPresent()) {
             log.info("Put coffee {} to Redis.", name);
+            // 将结果放入redis中
             hashOperations.put(CACHE, name, coffee.get());
+            // 设置过期时间
             redisTemplate.expire(CACHE, 1, TimeUnit.MINUTES);
         }
         return coffee;
