7b92eb3382e31c35e0781bef764f59a19b5ef710
taiyongyu <372858845@qq.com>
2019-04-25 17:29:14 +0800

    simple reactor

--- Chapter 5/simple-reactor-demo/src/main/java/geektime/spring/reactor/simple/SimpleReactorDemoApplication.java
+++ Chapter 5/simple-reactor-demo/src/main/java/geektime/spring/reactor/simple/SimpleReactorDemoApplication.java
@@ -19,28 +19,12 @@ public class SimpleReactorDemoApplication implements ApplicationRunner {
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
-		Flux.range(1, 6)
-				.doOnRequest(n -> log.info("Request {} number", n)) // 注意顺序造成的区别
-//				.publishOn(Schedulers.elastic())
-				.doOnComplete(() -> log.info("Publisher COMPLETE 1"))
-				.map(i -> {
-					log.info("Publish {}, {}", Thread.currentThread(), i);
-					return 10 / (i - 3);
-//					return i;
-				})
-				.doOnComplete(() -> log.info("Publisher COMPLETE 2"))
-//				.subscribeOn(Schedulers.single())
-//				.onErrorResume(e -> {
-//					log.error("Exception {}", e.toString());
-//					return Mono.just(-1);
-//				})
-//				.onErrorReturn(-1)
+		Flux.range(5, 6)
 				.subscribe(i -> log.info("Subscribe {}: {}", Thread.currentThread(), i),
 						e -> log.error("error {}", e.toString()),
-						() -> log.info("Subscriber COMPLETE")//,
+						() -> log.info("Subscriber COMPLETE") //,
 //						s -> s.request(4)
 				);
-		Thread.sleep(2000);
 	}
 }
 
