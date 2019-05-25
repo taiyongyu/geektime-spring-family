766f1a1c3104e67fb4959adbd315c3ad60760da1
taiyongyu <372858845@qq.com>
2019-05-25 11:11:52 +0800

    hateoas-waiter-service

--- Chapter 8/hateoas-waiter-service/src/main/java/geektime/spring/springbucks/waiter/WaiterServiceApplication.java
+++ Chapter 8/hateoas-waiter-service/src/main/java/geektime/spring/springbucks/waiter/WaiterServiceApplication.java
@@ -16,11 +16,22 @@ public class WaiterServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WaiterServiceApplication.class, args);
 	}
+
+	/**
+	 *  注册Hibernate5的支持
+	 * @return
+	 */
 	@Bean
 	public Hibernate5Module hibernate5Module() {
 		return new Hibernate5Module();
 	}
 
+	/**
+	 * 对JacksonBuilder做定制
+	 * 1、返回的json进行了格式化，带有缩进
+	 * 2、指定时区
+	 * @return
+	 */
 	@Bean
 	public Jackson2ObjectMapperBuilderCustomizer jacksonBuilderCustomizer() {
 		return builder -> {
@@ -28,4 +39,8 @@ public class WaiterServiceApplication {
 			builder.timeZone(TimeZone.getTimeZone("Asia/Shanghai"));
 		};
 	}
+
+	/**
+	 * 启动程序后，
+	 */
 }
--- Chapter 8/hateoas-waiter-service/src/main/java/geektime/spring/springbucks/waiter/repository/CoffeeRepository.java
+++ Chapter 8/hateoas-waiter-service/src/main/java/geektime/spring/springbucks/waiter/repository/CoffeeRepository.java
@@ -2,12 +2,22 @@ package geektime.spring.springbucks.waiter.repository;
 
 import geektime.spring.springbucks.waiter.model.Coffee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+/**
+ * 使用Spring Data Rest注解，指明path路径
+ * 利用Spring Data JPA，自定义了两个查询方法
+ */
 @RepositoryRestResource(path = "/coffee")
 public interface CoffeeRepository extends JpaRepository<Coffee, Long> {
-    List<Coffee> findByNameInOrderById(List<String> list);
-    Coffee findByName(String name);
+    List<Coffee> findByNameInOrderById(@Param(value = "list")List<String> list);
+    Coffee findByName(@Param(value = "name") String name);
+
+    /**
+     * 注意，在源代码中findByName方法并没有@Param(value = "name")
+     * 但是在实际运行中代码会报错，必须添加上@Param注解才可以
+     */
 }
