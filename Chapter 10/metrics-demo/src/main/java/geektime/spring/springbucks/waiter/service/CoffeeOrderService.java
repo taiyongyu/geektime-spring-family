ccb3d25c51f6aca0931b4b136e18efb32594d239
taiyongyu <372858845@qq.com>
2019-06-12 17:12:52 +0800

    metrics-demo

--- Chapter 10/metrics-demo/src/main/java/geektime/spring/springbucks/waiter/service/CoffeeOrderService.java
+++ Chapter 10/metrics-demo/src/main/java/geektime/spring/springbucks/waiter/service/CoffeeOrderService.java
@@ -15,6 +15,10 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+/**
+ * 自定义度量指标
+ * 实现MeterBinder接口
+ */
 @Service
 @Transactional
 @Slf4j
@@ -36,7 +40,10 @@ public class CoffeeOrderService implements MeterBinder {
                 .build();
         CoffeeOrder saved = orderRepository.save(order);
         log.info("New Order: {}", saved);
+
+        //每次创建订单的时候，计数器都+1
         orderCounter.increment();
+
         return saved;
     }
 
@@ -51,8 +58,18 @@ public class CoffeeOrderService implements MeterBinder {
         return true;
     }
 
+    /**
+     * 重写bindTo接口
+     * @param meterRegistry
+     */
     @Override
     public void bindTo(MeterRegistry meterRegistry) {
+        // 注册了一个名叫order.count的counter(计数器)
         this.orderCounter = meterRegistry.counter("order.count");
+        // 在浏览器访问 http://localhost:8080/actuator/metrics/order.count 即可查看该计数器的变化
+        // 也可以利用prometheus来查看该计数器：
+        // 访问：http://localhost:8080/actuator/prometheus 找到order_count_total即可。这是prometheus自动做了名称的转换
+
+        // 访问http://localhost:8080/actuator/metrics 可以查看其余的指标，例如访问次数的统计等等。
     }
 }
