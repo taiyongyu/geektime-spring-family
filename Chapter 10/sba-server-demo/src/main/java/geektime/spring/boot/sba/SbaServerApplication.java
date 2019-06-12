50b9809e14ef80fc6f7ebcaf25093a2068d1e3ef
datamanager <sunyu@victorysoft.com.cn>
2019-06-12 23:02:44 +0800

    spring boot admin

--- Chapter 10/sba-server-demo/src/main/java/geektime/spring/boot/sba/SbaServerApplication.java
+++ Chapter 10/sba-server-demo/src/main/java/geektime/spring/boot/sba/SbaServerApplication.java
@@ -10,6 +10,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
+/**
+ * spring boot admin server
+ * 使用@EnableAdminServer注解
+ * 集成WebSecurityConfigurerAdapter类
+ *
+ * 把server端和client端启动之后  server端会显示 spring boot admin的主界面
+ * 会发现有client端注册到server里面，就可以观察client端的各种运行指标
+ */
 @SpringBootApplication
 @EnableAdminServer
 public class SbaServerApplication extends WebSecurityConfigurerAdapter {
@@ -19,7 +27,8 @@ public class SbaServerApplication extends WebSecurityConfigurerAdapter {
 	public static void main(String[] args) {
 		SpringApplication.run(SbaServerApplication.class, args);
 	}
-
+    // 重写configure接口
+	// 具体还需要查一下 spring boot security的用法
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		String adminContextPath = adminServerProperties.getContextPath();
@@ -29,7 +38,7 @@ public class SbaServerApplication extends WebSecurityConfigurerAdapter {
 		successHandler.setDefaultTargetUrl(adminContextPath + "/");
 
 		http.authorizeRequests()
-				.antMatchers(adminContextPath + "/assets/**").permitAll()
+				.antMatchers(adminContextPath + "/assets/**").permitAll() // assets和login两个请求不需要进行验证
 				.antMatchers(adminContextPath + "/login").permitAll()
 				.anyRequest().authenticated()
 				.and()
