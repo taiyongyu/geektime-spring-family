3125a828096bb4fe6497c878bdfd1f470e702881
taiyongyu <372858845@qq.com>
2019-06-22 11:55:33 +0800

    hystrix dashboard demo

--- Chapter 13/hystrix-dashboard-demo/src/main/java/geektime/spring/cloud/hystrix/HystrixDashboardDemoApplication.java
+++ Chapter 13/hystrix-dashboard-demo/src/main/java/geektime/spring/cloud/hystrix/HystrixDashboardDemoApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
+/**
+ * 使用@EnableHystrixDashboard开启监控
+ */
 @SpringBootApplication
 @EnableHystrixDashboard
 public class HystrixDashboardDemoApplication {
--- Chapter 13/turbine-demo/src/main/java/geektime/spring/cloud/turbine/TurbineDemoApplication.java
+++ Chapter 13/turbine-demo/src/main/java/geektime/spring/cloud/turbine/TurbineDemoApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
+/**
+ * 使用@EnableTurbine接口开启即可
+ */
 @SpringBootApplication
 @EnableTurbine
 public class TurbineDemoApplication {
