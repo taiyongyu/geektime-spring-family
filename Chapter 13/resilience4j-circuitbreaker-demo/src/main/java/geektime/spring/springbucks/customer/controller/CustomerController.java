6c7214015c74ee65d2f7fb7fca21883279fc6a9e
taiyongyu <372858845@qq.com>
2019-06-22 15:14:48 +0800

    resilience4j-circuitbreaker-demo

--- Chapter 13/resilience4j-circuitbreaker-demo/src/main/java/geektime/spring/springbucks/customer/controller/CustomerController.java
+++ Chapter 13/resilience4j-circuitbreaker-demo/src/main/java/geektime/spring/springbucks/customer/controller/CustomerController.java
@@ -20,6 +20,9 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * 在该类中进行熔断演示
+ */
 @RestController
 @RequestMapping("/customer")
 @Slf4j
@@ -28,14 +31,22 @@ public class CustomerController {
     private CoffeeService coffeeService;
     @Autowired
     private CoffeeOrderService coffeeOrderService;
+
     private CircuitBreaker circuitBreaker;
 
+    /**
+     * 熔断方式二：
+     *  采用构造成员变量的形式，构造一个circuitBreaker，其name为menu
+     * @param registry
+     */
     public CustomerController(CircuitBreakerRegistry registry) {
         circuitBreaker = registry.circuitBreaker("menu");
     }
 
     @GetMapping("/menu")
     public List<Coffee> readMenu() {
+        // 为coffeeService.getAll()提供熔断保护
+        // 并且当发生异常时，直接返回一个空list
         return Try.ofSupplier(
                 CircuitBreaker.decorateSupplier(circuitBreaker,
                         () -> coffeeService.getAll()))
@@ -43,6 +54,13 @@ public class CustomerController {
                 .get();
     }
 
+    /**
+     * 熔断方式一：
+     *  如果熔断后不需要进行fallback特殊处理，可以采用引入全限定的方式，引入CircuitBreaker
+     *  然后给该熔断器起个名字，如order
+     *  则该createOrder方法收到保护。
+     * @return
+     */
     @PostMapping("/order")
     @io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker(name = "order")
     public CoffeeOrder createOrder() {
