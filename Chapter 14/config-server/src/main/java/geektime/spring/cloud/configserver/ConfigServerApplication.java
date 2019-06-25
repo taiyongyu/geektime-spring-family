94755e3b31a787737e2a67c070079670717331b9
taiyongyu <372858845@qq.com>
2019-06-25 17:22:02 +0800

    config server demo

--- Chapter 14/config-server/src/main/java/geektime/spring/cloud/configserver/ConfigServerApplication.java
+++ Chapter 14/config-server/src/main/java/geektime/spring/cloud/configserver/ConfigServerApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * 使用@EnableConfigServer注解，开启配置中心
+ */
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
